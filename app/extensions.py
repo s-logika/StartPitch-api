75880139463d9957356361f68a64e3a945b9b408
s-logika <273270343+s-logika@users.noreply.github.com>
2026-07-15 13:31:21 +0530

    add SQLAlchemy and Flask-Migrate extensions for database integration

--- app/extensions.py
+++ app/extensions.py
@@ -1,7 +1,11 @@
 from flask_bcrypt import Bcrypt
 from flask_cors import CORS
 from flask_jwt_extended import JWTManager
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
 
 bcrypt = Bcrypt()
 jwt = JWTManager()
 cors = CORS()
+db = SQLAlchemy()
+migrate = Migrate()
